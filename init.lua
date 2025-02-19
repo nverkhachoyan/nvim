a79510715ebc59d2f1667e067db85021285bab25
Nver Khachoyan <khachoyannver@gmail.com>
2025-02-19 01:44:46 -0800

    minimal setup

--- init.lua
+++ init.lua
@@ -1,2 +1,282 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Basic options
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.number = true         -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.mouse = 'a'          -- Enable mouse support
+vim.opt.ignorecase = true    -- Case insensitive search
+vim.opt.smartcase = true     -- But sensitive when includes uppercase
+vim.opt.hlsearch = false     -- Don't highlight search results
+vim.opt.wrap = false         -- Don't wrap lines
+vim.opt.breakindent = true   -- Maintain indent when wrapping
+vim.opt.tabstop = 2          -- Number of spaces tabs count for
+vim.opt.shiftwidth = 2       -- Size of an indent
+vim.opt.expandtab = true     -- Use spaces instead of tabs
+vim.opt.termguicolors = true -- True color support
+vim.opt.updatetime = 250     -- Decrease update time
+vim.opt.timeoutlen = 300     -- Time to wait for mapped sequence
+vim.opt.splitright = true    -- Put new windows right of current
+vim.opt.splitbelow = true    -- Put new windows below current
+vim.opt.scrolloff = 8        -- Lines of context
+vim.opt.sidescrolloff = 8    -- Columns of context
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
+
+-- Install package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin specifications
+require("lazy").setup({
+  -- Theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        color_overrides = {
+          mocha = {
+            base = "#202328",
+            text = "#bbc2cf",
+            blue = "#51afef",
+            red = "#ec5f67",
+            green = "#98be65",
+            yellow = "#ECBE7B",
+            magenta = "#c678dd",
+          },
+        },
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+
+  -- File finder
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+    end,
+  },
+
+  -- LSP Support
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+  },
+
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+    },
+  },
+
+  -- Status line
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local colors = {
+        bg = '#202328',
+        fg = '#bbc2cf',
+        yellow = '#ECBE7B',
+        cyan = '#008080',
+        darkblue = '#081633',
+        green = '#98be65',
+        orange = '#FF8800',
+        violet = '#a9a1e1',
+        magenta = '#c678dd',
+        blue = '#51afef',
+        red = '#ec5f67',
+      }
+
+      local conditions = {
+        buffer_not_empty = function()
+          return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+        end,
+        hide_in_width = function()
+          return vim.fn.winwidth(0) > 80
+        end,
+      }
+
+      local config = {
+        options = {
+          component_separators = '',
+          section_separators = '',
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          },
+          theme = {
+            normal = { c = { fg = colors.fg, bg = colors.bg } },
+            inactive = { c = { fg = colors.fg, bg = colors.bg } },
+          },
+        },
+        sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          lualine_c = {},
+          lualine_x = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          lualine_c = {},
+          lualine_x = {},
+        },
+      }
+
+      -- Inserts a component in lualine_c at left section
+      local function ins_left(component)
+        table.insert(config.sections.lualine_c, component)
+      end
+
+      -- Inserts a component in lualine_x at right section
+      local function ins_right(component)
+        table.insert(config.sections.lualine_x, component)
+      end
+
+      -- Left sections
+      ins_left {
+        'mode',
+        color = { fg = colors.blue, gui = 'bold' },
+      }
+
+      ins_left {
+        'filename',
+        cond = conditions.buffer_not_empty,
+        color = { fg = colors.magenta, gui = 'bold' },
+      }
+
+      ins_left {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        symbols = { error = 'E:', warn = 'W:', info = 'I:' },
+        diagnostics_color = {
+          error = { fg = colors.red },
+          warn = { fg = colors.yellow },
+          info = { fg = colors.blue },
+        },
+      }
+
+      -- Right sections
+      ins_right {
+        'branch',
+        color = { fg = colors.magenta, gui = 'bold' },
+      }
+
+      ins_right {
+        'diff',
+        symbols = { added = '+', modified = '~', removed = '-' },
+        diff_color = {
+          added = { fg = colors.green },
+          modified = { fg = colors.yellow },
+          removed = { fg = colors.red },
+        },
+        cond = conditions.hide_in_width,
+      }
+
+      ins_right {
+        'filetype',
+        color = { fg = colors.green, gui = 'bold' },
+      }
+
+      require('lualine').setup(config)
+    end,
+  },
+
+  -- Syntax highlighting
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+  },
+})
+
+-- LSP Configuration
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer" },
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- LSP servers setup
+require("lspconfig").lua_ls.setup({
+  capabilities = capabilities,
+})
+require("lspconfig").ts_ls.setup({
+  capabilities = capabilities,
+})
+
+-- Completion setup
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Treesitter setup
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { "lua", "javascript", "typescript", "rust" },
+  highlight = {
+    enable = true,
+  },
+})
+
+-- Basic keymaps
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
+vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>', { desc = 'Quit' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<cr>', { desc = 'Clear search highlight' })
+
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to upper window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
